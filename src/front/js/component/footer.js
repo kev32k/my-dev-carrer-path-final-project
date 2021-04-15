3a5f09e37572d09186e301ca973c612179d87530
Walt Castillo <walt.castillo@gmail.com>
2021-04-15 17:08:15 +0000

    footer update

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -3,18 +3,20 @@ import React, { Component } from "react";
 export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center">
 		<div className="container p-4 pb-0">
-      <section className="">
-        <p className="d-flex justify-content-center align-items-center">
-          <span className="me-3">Register for free</span>
-          <button type="button" className="btn btn-outline-light btn-rounded">
-            Sign up!
-          </button>
-        </p>
-      </section>
-    </div>
-    <div className="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-      © 2021 Copyright:
-      <a class="text-white" href="https://mydevcareerpath.com/">MyDevCareerPath.com</a>
-    </div>
+			<section className="">
+				<p className="d-flex justify-content-center align-items-center">
+					<span className="me-3">Register for free</span>
+					<button type="button" className="btn btn-outline-light btn-rounded">
+						Sign up!
+					</button>
+				</p>
+			</section>
+		</div>
+		<div className="text-center p-3">
+			© 2021 Copyright:
+			<a className="text-white" href="https://mydevcareerpath.com/">
+				MyDevCareerPath.com
+			</a>
+		</div>
 	</footer>
 );
