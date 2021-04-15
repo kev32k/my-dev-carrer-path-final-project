8f4cbf675f011ecf8d0e044143b4c3745adc405c
Jorge R <jorgebeto00@protonmail.com>
2021-04-15 19:10:24 +0000

    added card component

---
+++ src/front/js/component/card.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const Card = () => {
+    return(
+        <div className="card" style="width: 18rem;">
+  <img src="..." className="card-img-top" alt="..."/>
+  <div className="card-body">
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
+  </div>
+</div>
+    )
+}
