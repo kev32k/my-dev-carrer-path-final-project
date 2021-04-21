9845f6093cd0dc46a339c630054ad6c70b7558fc
Jorge R <jorgebeto00@protonmail.com>
2021-04-21 16:12:50 +0000

    learningpathview

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -9,6 +9,7 @@ import { Demo } from "./pages/demo";
 import { Single } from "./pages/single";
 import injectContext from "./store/appContext";
 import { LearningPath } from "./pages/learningPath";
+import { LearningPathView } from "./pages/learningPathView";
 import { User_Dashboard } from "./pages/userdashboard";
 
 
@@ -34,7 +35,9 @@ const Layout = () => {
 						<Route exact path="/learningPath">
 							<LearningPath />
 						</Route>
-						
+                        <Route exact path="/learningPathView">
+							<LearningPathView />
+						</Route>
 						<Route exact path="/login">
 							<Login />
 						</Route>
