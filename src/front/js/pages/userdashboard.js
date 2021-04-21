b5c0d0cc670fdc17c5ed344b9d3adb9f941c3840
Jorge R <jorgebeto00@protonmail.com>
2021-04-21 21:47:28 +0000

    aa

---
+++ src/front/js/pages/userdashboard.js
@@ -0,0 +1,43 @@
+import React from "react";
+import "../../styles/userdashboard.scss";
+import profileImageUrl from "../../img/avatar-profile.png";
+
+export const User_Dashboard = () => {
+	return (
+		<div className="container">
+			<div className="main row">
+				<div className="col md-9">
+					<div className="row gutters-sm">
+						<div className="col-md-4 mb-3">
+							<div className="card">
+								<div className="card-body">
+									<div className="d-flex flex-column align-items-center text-center">
+										<img img src={profileImageUrl} alt="Admin" className="" />
+										<div className="mt-3">
+											<button className="btn btn-outline-primary">Change Picture</button>
+											<p className="text-secondary mb-1">The Bookmark Keeper</p>
+											<p className="text-muted font-size-sm">Change Password</p>
+											<button className="btn btn-outline-primary">Logout</button>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="col-md-8">
+					<div className="card mb-3">
+						<div className="card-body">
+							<div className="row">
+								<div className="col-sm-5">
+									<p className="mb">My Carrer Path</p>
+									<button className="btn btn-card">⭐Front-end Developer</button>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
