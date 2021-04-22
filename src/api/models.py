ffef7c81c891329bd376319d3d8194874a6b4361
Walt Castillo <walt.castillo@gmail.com>
2021-04-22 18:53:24 +0000

    models

--- src/api/models.py
+++ src/api/models.py
@@ -4,9 +4,38 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    #career_links = db.relationship('CareerLink', backref='user', lazy=True)
+
+class UserPath(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.String(120), db.ForeignKey('User.id'), unique=True, nullable=False)
+    careerpath_id = db.Column(db.String(120), db.ForeignKey('CareerPath.id'), unique=True, nullable=False)
+
+class CareerPath(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+
+class CareerSkills(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    careerpath_id = db.Column(db.String(120), db.ForeignKey('CareerPath.id'), unique=True, nullable=False)
+    skill_id = db.Column(db.String(120), db.ForeignKey('Skill.id'), unique=True, nullable=False)
+
+class Skill(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    #career_links = db.relationship('CareerLink', backref='Skill', lazy=True)
+
+class CareerLink(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    link = db.Column(db.String(120), unique=True, nullable=False)
+    status = db.Column(db.String(80), unique=False, nullable=False)
+    user_id = db.Column(db.Boolean(), db.ForeignKey('User.id'), unique=False, nullable=False)
+    skill_id = db.Column(db.Boolean(), db.ForeignKey('Skill.id'), unique=False, nullable=False)
 
     def __repr__(self):
         return '<User %r>' % self.username
